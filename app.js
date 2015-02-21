5cffd2540960df6118e9054fd24b646fb8d171bf
Eli Duke <elimduke@gmail.com>
2015-02-20 18:05:07 -0800

    removes js toggle for code of conduct

--- app.js
+++
@@ -1,6 +0,0 @@
-$('#code_of_conduct_button').click(function() {
-    $('#code_of_conduct').toggle();
-    $('.code_of_conduct_link').toggle();
-    $('html,body').animate({scrollTop: $('#code').offset().top},'slow');
-    return false;
-})
